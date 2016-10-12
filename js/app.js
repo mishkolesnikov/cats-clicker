5c22bd76bfec5bf3970468c8ce3afc701995e2d1
mkolesnikov <mishkolesnikov@gmail.com>
2016-10-12 18:57:43 +0300

    first attempt

--- js/app.js
+++ js/app.js
@@ -1,4 +1,15 @@
-(function(){
-    var divElem = document.getElementById("cats-area");
-    devElem.text = "qwe";
+;(function(){
+    function Cat(name){
+        this.name = name;
+        this.clickCount = 0;
+        this.clickCounter = function(){
+            this.clickCount++;
+            this.text.innerHTML = this.name + " : " + this.clickCount;
+        }
+    }
+    window.badCat = new Cat("badcat");
+    window.goodCat = new Cat("goodcat");
+
+    window.badCat.text = document.getElementById("badcattext");
+    window.goodCat.text = document.getElementById("goodcattext");
 })();
