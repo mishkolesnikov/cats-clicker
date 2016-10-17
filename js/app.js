1fd375499148aa3ce2686e59157159b96276ff5b
mkolesnikov <mishkolesnikov@gmail.com>
2016-10-17 20:09:22 +0300

    step 2.1

--- js/app.js
+++ js/app.js
@@ -1,15 +1,41 @@
-;(function(){
-    function Cat(name){
+;(function(){    
+    var catsArea = document.getElementById('cats-area');
+    var catsList = document.getElementById('cats-list');
+    function Cat(name, avatarLink){
         this.name = name;
         this.clickCount = 0;
         this.clickCounter = function(){
             this.clickCount++;
-            this.text.innerHTML = this.name + " : " + this.clickCount;
-        }
-    }
-    window.badCat = new Cat("badcat");
-    window.goodCat = new Cat("goodcat");
+            catText.innerHTML = this.name + '\n' + 'got ' + this.clickCount;
+       }.bind(this);
+       this.displayCat = function(){
+            catsArea.innerHTML = '';
+            catsArea.appendChild(catInfo);
+       }.bind(this);
+    //формируем блок информации о коте, с обработкой клика по картинке
+    var catInfo = document.createElement('div');
+    catInfo.className = 'cat';
 
-    window.badCat.text = document.getElementById("badcattext");
-    window.goodCat.text = document.getElementById("goodcattext");
+    var catPicture = document.createElement('img');
+    catPicture.setAttribute('src', avatarLink);
+    catPicture.addEventListener("click", this.clickCounter);
+    
+    var catText = document.createElement('p');
+    catText.innerHTML = this.name + '\n' + 'got ' + this.clickCount;
+
+    catInfo.appendChild(catPicture);
+    catInfo.appendChild(catText);
+
+    //добавляем кота в список, с обработкой клика
+    var catLi = document.createElement('li');
+    catLi.innerHTML = this.name;
+    catsList.appendChild(catLi);
+    catLi.addEventListener("click", this.displayCat);
+    }
+
+
+    badCat = new Cat("Baddy", "img/badcat.jpg");
+    goodCat = new Cat("Gooddy", "img/Goodcat.jpg");
+    angryCat = new Cat("Angrys", "img/angry.jpg");
+    badCat.displayCat();
 })();
